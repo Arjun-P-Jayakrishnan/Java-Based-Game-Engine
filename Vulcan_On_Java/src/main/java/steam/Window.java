08d40526f4be94d879a88addef6f48b67c469aab
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2025-02-05 20:38:42 +0530

    :tada: start the vulcan engine

---
+++ Vulcan_On_Java/src/main/java/steam/Window.java
@@ -0,0 +1,13 @@
+package steam;
+
+public class Window {
+    int width;
+    int height;
+
+    String title;
+    private Window(){
+        this.width=1920;
+        this.height=1080;
+        title="Mario";
+    }
+}
