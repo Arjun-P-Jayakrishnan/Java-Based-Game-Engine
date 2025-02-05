ff26ce5d80a91e2d15944c4de85640159b50d4f9
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2025-02-05 20:54:36 +0530

    :pencil2: some updates in code with loop and init

--- Vulcan_On_Java/src/main/java/steam/Window.java
+++ Vulcan_On_Java/src/main/java/steam/Window.java
@@ -1,13 +1,41 @@
 package steam;
 
-public class Window {
-    int width;
-    int height;
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
 
-    String title;
+public class Window {
+    private int width;
+    private int height;
+
+    private String title;
+
+    private static Window window =null;
+
     private Window(){
         this.width=1920;
         this.height=1080;
         title="Mario";
+    }
+
+    public static Window get(){
+        if(Window.window==null){
+            Window.window=new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL"+ Version.getVersion()+"!");
+
+        init();
+        loop();
+    }
+
+    public void init(){
+        GLFWErrorCallback.createPrint(System.err).set();
+    }
+
+    public void loop(){
+
     }
 }
